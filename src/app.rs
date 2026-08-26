282fb2dc4173f56c08133f40e7673cac2befb112
free <mpdf2023@outlook.com>
2026-08-26 19:01:16 +0800

    feat(gui): a base gui

---
+++ src/app.rs
@@ -0,0 +1,51 @@
+mod fonts;
+
+use eframe::egui;
+
+use crate::app::Backend::Latex;
+use crate::app::Output::Pdf;
+
+#[derive(Default)]
+enum Backend{
+    #[default]
+    Latex,
+    Typst
+}
+
+#[derive(Default)]
+enum Output{
+    #[default]
+    Pdf,
+    Svg
+}
+
+pub struct MyApp{
+    pub backend: Backend,
+    pub output: Output,
+}
+
+impl Default for MyApp{
+    fn default() -> Self{
+        Self{
+            backend:Latex,
+            output:Pdf,
+        }
+    }
+}
+
+impl MyApp{
+    pub fn new(cc:&eframe::CreationContext<'_>) -> Self{
+        let fonts = fonts::setup_fonts();
+        cc.egui_ctx.set_fonts(fonts);
+
+        let mut app:MyApp = Default::default();
+
+        app
+    }
+}
+
+impl eframe::App for MyApp {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        ui.heading("Equation Exporter");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+
+use app::MyApp;
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result<()>{
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "equation-exporter",
+        options,
+        Box::new(|cc| Ok(Box::new(MyApp::new(cc)))),
+    )
 }
